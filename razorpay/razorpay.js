1c559ae5c84a19773a1105b31226ec76178ea678
Kavya <kavyarathod0507@gmail.com>
2024-12-25 15:25:44 +0530

    testing

--- razorpay/razorpay.js
+++ razorpay/razorpay.js
@@ -3,6 +3,65 @@ const { sendMessage } = require("../utils/whatsappAPI");
 const User = require('../models/User');
 
 // Function to calculate and create Razorpay payment link
+// exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, description = "Purchase at Nani's Bilona Ghee") => {
+//   const url = 'https://api.razorpay.com/v1/payment_links';
+//   const auth = {
+//     username: process.env.RAZORPAY_KEY_ID,
+//     password: process.env.RAZORPAY_KEY_SECRET,
+//   };
+
+//   // Calculate the amount by dividing by 500, then convert to paise (for Razorpay)
+//   const calculatedAmount = Math.round(amountEntered) * 100; // Amount in paise
+
+//   const user = await User.findOne({phone:userPhone})
+//   user.userOrderAmount = calculatedAmount/100;
+//   user.save()
+
+//   try {
+//     const response = await axios.post(
+//       url,
+//       {
+//         amount: calculatedAmount,
+//         currency: 'INR',
+//         description: description,
+//         customer: {
+//           contact: userPhone,
+//         },
+//         notify: {
+//           sms: true,
+//           email: true,
+//           whatsapp: true,
+//         },
+//         callback_url: process.env.CALLBACK_URL, // Update as needed
+//         callback_method: 'get',
+//       },
+//       { auth }
+//     );
+
+//     const paymentLink = response.data.short_url;
+    
+//     // Send success notification to the admin
+//     const adminPhone = process.env.ADMIN_PHONE ;
+//     const successMessage = {
+//       text: `💳 Payment link created for name:${user.name} phone *${userPhone}*:\n🔗 *${paymentLink}*\nThank you! 😊`,
+//     };
+//     await sendMessage(adminPhone, successMessage);
+
+//     return paymentLink;
+//   } catch (error) {
+
+//     // Send error message to the admin
+//     const adminPhone = process.env.ADMIN_PHONE || 'YOUR_ADMIN_PHONE_NUMBER';
+//     const errorMessage = {
+//       text: `Alert: Failed to create payment link for  name:${user.name} phone ${userPhone}. Error: ${error.response ? error.response.data.description : error.message}`,
+//     };
+//     await sendMessage(adminPhone, errorMessage);
+
+//     throw new Error('Failed to create payment link');
+//   }
+// };
+
+
 exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, description = "Purchase at Nani's Bilona Ghee") => {
   const url = 'https://api.razorpay.com/v1/payment_links';
   const auth = {
@@ -21,7 +80,7 @@ exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, descr
     const response = await axios.post(
       url,
       {
-        amount: calculatedAmount,
+        amount: 5,
         currency: 'INR',
         description: description,
         customer: {
