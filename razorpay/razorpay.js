6e110c65a54065a94391c7c534ef63b0e5f08584
Kavya <kavyarathod0507@gmail.com>
2025-01-04 22:07:20 +0530

    undo

--- razorpay/razorpay.js
+++ razorpay/razorpay.js
@@ -17,8 +17,7 @@ exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, descr
   const user = await User.findOne({phone:userPhone})
   user.userOrderAmount = calculatedAmount/100;
   user.save()
-  const fiveMinutesFromNow = Math.floor(Date.now() / 1000) + 5 * 60; // Current time + 5 minutes in seconds
-clog(fiveMinutesFromNow);
+
   try {
     const response = await axios.post(
       url,
@@ -36,12 +35,10 @@ clog(fiveMinutesFromNow);
         },
         callback_url: process.env.CALLBACK_URL, // Update as needed
         callback_method: 'get',
-     
       },
       { auth }
     );
-    console.log(response);
-    
+
     const paymentLink = response.data.short_url;
     
     // Send success notification to the admin
@@ -53,12 +50,13 @@ clog(fiveMinutesFromNow);
 
     return paymentLink;
   } catch (error) {
-    console.log(error);
+
+    // Send error message to the admin
     const adminPhone = process.env.ADMIN_PHONE || 'YOUR_ADMIN_PHONE_NUMBER';
     const errorMessage = {
       text: `Alert: Failed to create payment link for  name:${user.name} phone ${userPhone}. Error: ${error.response ? error.response.data.description : error.message}`,
     };
-   // await sendMessage(adminPhone, errorMessage);
+    await sendMessage(adminPhone, errorMessage);
 
     throw new Error('Failed to create payment link');
   }
