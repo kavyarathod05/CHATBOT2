9df9bf235992ce5b301b9aed102f750d1c645b0e
Kavya <kavyarathod0507@gmail.com>
2025-01-04 22:10:39 +0530

    expiry

--- razorpay/razorpay.js
+++ razorpay/razorpay.js
@@ -17,6 +17,7 @@ exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, descr
   const user = await User.findOne({phone:userPhone})
   user.userOrderAmount = calculatedAmount/100;
   user.save()
+  const fiveMinutesFromNow = Math.floor(Date.now() / 1000) + 5 * 60; // 5 minutes from now in Unix timestamp
 
   try {
     const response = await axios.post(
@@ -35,10 +36,12 @@ exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, descr
         },
         callback_url: process.env.CALLBACK_URL, // Update as needed
         callback_method: 'get',
+        expire_by: fiveMinutesFromNow
       },
       { auth }
     );
-
+    console.log(response);
+    
     const paymentLink = response.data.short_url;
     
     // Send success notification to the admin
@@ -50,7 +53,8 @@ exports.generatePaymentLinkWithDivision = async (amountEntered, userPhone, descr
 
     return paymentLink;
   } catch (error) {
-
+    console.log(error);
+    
     // Send error message to the admin
     const adminPhone = process.env.ADMIN_PHONE || 'YOUR_ADMIN_PHONE_NUMBER';
     const errorMessage = {
